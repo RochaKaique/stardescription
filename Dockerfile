4464128fa9efb6681ea4185a8f01b7f00316a062
Kaique Rocha <kaique@MacBook-Pro-de-Kaique.local>
2024-11-30 15:22:43 -0300

    chore: ajustando dockerfile, incluindo prometheus e healthcheck

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM golang:1.23
+FROM golang:1.23.3-alpine3.20 as go
 
-WORKDIR /usr/src/app
-
+WORKDIR /app
 COPY . .
-
 RUN go mod download && go mod verify
+RUN go build -o app
 
-RUN go build
+FROM alpine:latest
+
+WORKDIR /app
+RUN apk upgrade
+COPY --from=go app ./
 
 EXPOSE 8080
 
-CMD ["./bff"]
+ENTRYPOINT [ "./app" ]
--- src/router/router.go
+++ src/router/router.go
@@ -4,10 +4,19 @@ import (
 	"bff/src/controllers"
 	"bff/src/middleware"
 
+	"github.com/ansrivas/fiberprometheus/v2"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(app *fiber.App) {
+	prometheus := fiberprometheus.New("stardescription")
+	prometheus.RegisterAt(app, "/metrics")
+	prometheus.SetSkipPaths([]string{"/ping"})
+	
+	app.Use(prometheus.Middleware)
+	app.Use(healthcheck.New())
+
 	app.Get("/person", middleware.Authenticate, middleware.HandleLog, controllers.GetDescription)
 	app.Post("/user", controllers.CreateUser)
 	app.Post("/login", middleware.HandleLog, controllers.Login)
